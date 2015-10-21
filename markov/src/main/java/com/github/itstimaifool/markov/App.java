df51a31d55c42a4a3024e8a0da819eb4666c2011
Timmy <timjimski@gmail.com>
2015-10-21 16:29:58 -0400

    Initial Commit

---
+++ markov/src/main/java/com/github/itstimaifool/markov/App.java
@@ -0,0 +1,13 @@
+package com.github.itstimaifool.markov;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
